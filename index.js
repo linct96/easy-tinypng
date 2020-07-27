19ef16e3943241cc37b6d00503df3d3312ed4560
linchaoting <linchaoting@duiba.com.cn>
2020-07-27 23:43:27 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: linchaoting
+ * @Date: 2020-07-27 23:21:58
+ * @LastEditTime: 2020-07-27 23:21:59
+ */ 
+function hello(name){
+  console.log("hello "+ name);
+}
+exports.hello=hello;
