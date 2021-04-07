97f935819851cb6a3c42ff786a4e501de253b674
linct96 <314220356@qq.com>
2021-04-07 14:00:19 +0800

    feat:完成基本功能

--- bin/index.js
+++ bin/index.js
@@ -9,13 +9,16 @@ program.version(pkg.version);
 program.requiredOption('-i --input <path>', 'input file path')
 program.option('-o --output <path>', 'output file path')
 program.option('-r --ratio <number>', 'unCompress ratio limit')
+program.option('-v --verbose', 'show log')
 program.parse(process.argv)
 const {
   input: inputPath,
   output: outputPath,
   ratio: compressRatioLimit,
+  verbose: verbose
 } = program.opts()
 tiny(inputPath, {
   outputPath,
-  compressRatioLimit
+  compressRatioLimit,
+  verbose
 })
